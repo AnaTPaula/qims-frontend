baef4db14aa3d385164b9a7fd9e82ca819e8c98e
DESKTOP-22BMFRD\atere <aterezinha.19@hotmail.com>
2022-06-04 09:06:14 -0300

    Criar pagina funcionário

--- qim/src/app/pages/funcionario/funcionario-form/funcionario-form.component.ts
+++ qim/src/app/pages/funcionario/funcionario-form/funcionario-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Funcionario } from '../model';
+import { FuncionarioFormService } from './funcionario-form.service';
 
 @Component({
   selector: 'app-funcionario-form',
@@ -6,10 +9,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funcionario-form.component.css']
 })
 export class FuncionarioFormComponent implements OnInit {
+  empresaId: number | undefined;
+  funcionarioId: number | undefined;
+  funcionario = {} as Funcionario;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private funcionarioService: FuncionarioFormService) { 
+  }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.empresaId = params['empresaId'];
+      this.funcionarioId = params['id'];
+    });
+    if( this.empresaId && this.funcionarioId) {
+      this.funcionarioService.getFuncionario(this.empresaId, this.funcionarioId).subscribe((funcionario: Funcionario) => {
+        this.funcionario = funcionario;
+      });
+    } else {
+      this.funcionario.acesso = 'total'
+    }
+  }
+
+  save() {
+    if(this.funcionario.id) {
+      const funcionarioForUpdate = {
+        'id': this.funcionario.id,
+        'nomeUsuario': this.funcionario.nomeUsuario,
+        'senha': this.funcionario.senha,
+        'acesso': this.funcionario.acesso,
+        'empresaId': this.funcionario.empresaId
+      } as Funcionario;
+      this.funcionarioService.updateFuncionario(funcionarioForUpdate).subscribe(() => {
+          this.router.navigate(['/empresa/' + this.empresaId + '/funcionario'])
+      })
+    } else {
+      const funcionarioForCreate = {
+        'nomeUsuario': this.funcionario.nomeUsuario,
+        'senha': this.funcionario.senha,
+        'acesso': this.funcionario.acesso,
+        'empresaId': this.empresaId
+      } as Funcionario;
+      this.funcionarioService.createFuncionario(funcionarioForCreate).subscribe(() => {
+        this.router.navigate(['/empresa/' + this.empresaId + '/funcionario'])
+    })
+    }
   }
 
 }
--- qim/src/app/pages/funcionario/funcionario-list/funcionario-list.component.ts
+++ qim/src/app/pages/funcionario/funcionario-list/funcionario-list.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { CookieService } from 'ngx-cookie-service';
+import { EMPTY, switchMap, take } from 'rxjs';
+import { ConfirmModalComponent } from 'src/app/shared/confirm-modal/confirm-modal.component';
+import { Funcionario } from '../model';
+import { FuncionarioListService } from './funcionario-list.service';
 
 @Component({
   selector: 'app-funcionario-list',
@@ -6,10 +13,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funcionario-list.component.css']
 })
 export class FuncionarioListComponent implements OnInit {
+  empresaId: number | undefined;
+  funcionarios: Funcionario[] = [];
+  situacao: string | undefined;
 
-  constructor() { }
+  constructor(
+    private funcionarioService: FuncionarioListService,
+    private modalService: BsModalService,
+    private cookieService: CookieService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => this.empresaId = params['empresaId']);
+    this.getFuncionarios();
+    this.situacao = this.cookieService.get('situacao');
+  }
+
+  getFuncionarios() {
+    if (this.empresaId){
+      this.funcionarioService.getFuncionarios(this.empresaId).subscribe((funcionarios: Funcionario[]) => {
+        this.funcionarios = funcionarios;
+      });
+    }
+  }
+
+  convertDate(timestamp: number) {
+    if (timestamp) {
+      return new Date(timestamp * 1000).toLocaleDateString("pt-br");
+    } else {
+      return '';
+    }
+  }
+
+  convertAcesso(acesso: string) {
+    if(acesso === 'total') {
+      return 'Permissão Total';
+    } else if (acesso === 'modificar') {
+      return 'Permissão Alteração/Leitura';
+    } else if (acesso === 'leitura') {
+      return 'Permissão apenas Leitura';
+    } else {
+      return acesso;
+    }
+  }
+
+  deleteFuncionario(funcionario: Funcionario) {
+    const result$ = this.showConfirm(
+      'Deletar Funcionário',
+      'Tem certeza que deseja deletar o(a) funcionário(a) ' + funcionario.nomeUsuario + '?',
+      'Cancelar',
+      'Deletar');
+    result$?.asObservable()
+      .pipe(
+        take(1),
+        switchMap(response => response ? this.funcionarioService.deleteFuncionario(funcionario) : EMPTY)
+      ).subscribe(() => {
+        this.getFuncionarios();
+      });
+  }
+
+  showConfirm(title: string, body: string, cancel: string, confirm: string) {
+    const bsModalRef: BsModalRef = this.modalService.show(ConfirmModalComponent);
+    bsModalRef.content.title = title;
+    bsModalRef.content.body = body;
+    bsModalRef.content.cancel = cancel;
+    bsModalRef.content.confirm = confirm;
+    bsModalRef.content.buttomClass = 'btn-danger';
+    return (<ConfirmModalComponent>bsModalRef.content).confirmResult;
   }
 
 }
