a018315d1b23901233a71f9a9d0af7b01f6be4da
DESKTOP-22BMFRD\atere <aterezinha.19@hotmail.com>
2022-06-04 09:02:26 -0300

    Alterar componentes principais da aplicação

--- qim/src/app/app.module.ts
+++ qim/src/app/app.module.ts
@@ -1,15 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from '@angular/common/http'
-
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { CookieService } from 'ngx-cookie-service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmpresaFormComponent } from './pages/empresa/empresa-form/empresa-form.component';
-import { EmpresaListComponent } from './pages/empresa/empresa-list/empresa-list.component';
-import { FuncionarioListComponent } from './pages/funcionario/funcionario-list/funcionario-list.component';
-import { FuncionarioFormComponent } from './pages/funcionario/funcionario-form/funcionario-form.component';
-import { AlmoxarifadoListComponent } from './pages/almoxarifado/almoxarifado-list/almoxarifado-list.component';
+import { AuthGuardService } from './guards/auth-guard.service';
 import { AlmoxarifadoFormComponent } from './pages/almoxarifado/almoxarifado-form/almoxarifado-form.component';
+import { UniqueAlmoxarifadoDirective } from './pages/almoxarifado/almoxarifado-form/unique-almoxarifado.directive';
+import { AlmoxarifadoListComponent } from './pages/almoxarifado/almoxarifado-list/almoxarifado-list.component';
+import { EmpresaFormComponent } from './pages/empresa/empresa-form/empresa-form.component';
+import { UniqueEmpresaDirective } from './pages/empresa/empresa-form/unique-empresa.directive';
+import { EmpresaListComponent } from './pages/empresa/empresa-list/empresa-list.component';
+import { FuncionarioFormComponent } from './pages/funcionario/funcionario-form/funcionario-form.component';
+import { UniqueFuncionarioDirective } from './pages/funcionario/funcionario-form/unique-funcionario.directive';
+import { FuncionarioListComponent } from './pages/funcionario/funcionario-list/funcionario-list.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ConfirmModalComponent } from './shared/confirm-modal/confirm-modal.component';
+import { HomeComponent } from './pages/home/home/home.component';
 
 
 @NgModule({
@@ -20,14 +29,26 @@ import { AlmoxarifadoFormComponent } from './pages/almoxarifado/almoxarifado-for
     FuncionarioListComponent,
     FuncionarioFormComponent,
     AlmoxarifadoListComponent,
-    AlmoxarifadoFormComponent
+    AlmoxarifadoFormComponent,
+    LoginComponent,
+    ConfirmModalComponent,
+    UniqueAlmoxarifadoDirective,
+    UniqueFuncionarioDirective,
+    UniqueEmpresaDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    FormsModule,
+    HttpClientModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    LoginComponent, 
+    CookieService,
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
