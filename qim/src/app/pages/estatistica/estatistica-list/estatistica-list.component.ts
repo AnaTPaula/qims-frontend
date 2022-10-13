b2ca30d7b7e4ac56a2b65247da5d921ae8b1b59e
DESKTOP-22BMFRD\atere <aterezinha.19@hotmail.com>
2022-10-13 20:04:41 -0300

    Desenvolver a tela de home e criar gráficos de estatistica

--- qim/src/app/pages/estatistica/estatistica-list/estatistica-list.component.ts
+++ qim/src/app/pages/estatistica/estatistica-list/estatistica-list.component.ts
@@ -4,6 +4,7 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { CookieService } from 'ngx-cookie-service';
 import { Estatistica } from '../model';
 import { EstatisticaListService } from './estatistica-list.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-estatistica-list',
@@ -16,7 +17,7 @@ export class EstatisticaListComponent implements OnInit {
   estatistica: Estatistica = {} as Estatistica;
   paginaAtual = 1;
   requestFailed: boolean = false;
-  
+
   constructor(
     private estatisticaService: EstatisticaListService,
     private modalService: BsModalService,
@@ -33,16 +34,18 @@ export class EstatisticaListComponent implements OnInit {
   }
 
   getEstatistica() {
-    if (this.empresaId){
+    if (this.empresaId) {
       this.estatisticaService.getEstatistica(this.empresaId).subscribe((estatistica: Estatistica) => {
-        this.estatistica= estatistica;
+        this.estatistica = estatistica;
+        this.chartEntrada();
+        this.chartSaida();
       });
     }
   }
 
   convertDate(timestamp: number) {
     if (timestamp) {
-      return new Date(timestamp * 1000).toLocaleDateString("pt-br");
+      return new Date(timestamp * 1000).toLocaleDateString("pt-br") + " " + new Date(timestamp * 1000).toLocaleTimeString("pt-br");
     } else {
       return '';
     }
@@ -50,6 +53,66 @@ export class EstatisticaListComponent implements OnInit {
 
   checkError() {
     this.requestFailed = false;
+  }
+
+  chartEntrada() {
+    let labels = [];
+    let values = [];
+    for (let entrada of this.estatistica.entradaProduto) {
+      labels.push(entrada.nome);
+      values.push(entrada.quantidade);
+    }
+    var chartEntrada: any = document.getElementById('chart-entrada');
+    /*var ordersChart = new Chart(chartEntrada, {
+      type: 'bar',
+      options: chartExample2.options,
+      data: chartExample2.data
+    });*/
+   
+    var barColors = ["red", "green", "blue", "orange", "brown"];
+    new Chart(chartEntrada, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            data: values,
+            maxBarThickness: 10,
+            backgroundColor: 'rgba(255, 0, 0, 0.1)',
+            
+          }
+        ]
+      },
+      options: {
+        legend: { display: false }
+      }
+    })
+  }
+
+  chartSaida() {
+    let labels = [];
+    let values = [];
+    for (let saida of this.estatistica.saidaProduto) {
+      labels.push(saida.nome);
+      values.push(saida.quantidade);
+    }
+    var chartEntrada: any = document.getElementById('chart-saida');
+    new Chart(chartEntrada, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            data: values,
+            maxBarThickness: 10,
+            backgroundColor: 'rgba(255, 0, 0, 0.1)'
+          }
+        ]
+      },
+      options: {
+        legend: { display: false },
+      }
+    })
   }
 
 }
--- qim/src/app/pages/historico/model.ts
+++ qim/src/app/pages/historico/model.ts
@@ -2,6 +2,7 @@ export interface Historico {
     operador: string;
     nomeProduto: string ;
     estoque: string;
+    estoqueDestino?: string;
     quantidade: number;
     empresaId: number;
     operacao: string;
